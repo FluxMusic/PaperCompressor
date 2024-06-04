6dce0f1cb76fd605f3c2b8825e3ba56fbc6de10b
FluxMusic <alex.huber743@gmail.com>
2024-06-04 22:09:19 +0200

    added basic layout skeleton

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -11,11 +11,24 @@
 
 //==============================================================================
 PaperCompressorAudioProcessorEditor::PaperCompressorAudioProcessorEditor (PaperCompressorAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+    : AudioProcessorEditor (&p), audioProcessor (p),
+attackSlider(*audioProcessor.getAPVTS().getParameter("Attack"), "Attack"),
+ratioSlider(*audioProcessor.getAPVTS().getParameter("Ratio"), "Ratio"),
+releaseSlider(*audioProcessor.getAPVTS().getParameter("Release"), "Release"),
+attackSliderAttachment(audioProcessor.getAPVTS(), "Attack", attackSlider),
+ratioSliderAttachment(audioProcessor.getAPVTS(), "Ratio", ratioSlider),
+releaseSliderAttachment(audioProcessor.getAPVTS(), "Release", releaseSlider),
+outputComponent(p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    addAndMakeVisible(outputComponent);
+    addAndMakeVisible(attackSlider);
+    addAndMakeVisible(ratioSlider);
+    addAndMakeVisible(releaseSlider);
+    
+    setSize (500, 330);
+    setResizable(true, true);
+    getConstrainer()->setFixedAspectRatio(1.52f);
+    setResizeLimits(500, 330, 1000, 660);
 }
 
 PaperCompressorAudioProcessorEditor::~PaperCompressorAudioProcessorEditor()
@@ -25,16 +38,48 @@ PaperCompressorAudioProcessorEditor::~PaperCompressorAudioProcessorEditor()
 //==============================================================================
 void PaperCompressorAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll (juce::Colours::floralwhite);
+    
+    auto bounds = getLocalBounds().toFloat();
+    bounds = bounds.reduced(bounds.getWidth() / 60);
+    
+    g.setColour(juce::Colours::black);
+    g.drawRoundedRectangle(bounds, 5.f, bounds.getHeight() / 130);
+    
+    auto outputBounds = bounds.removeFromRight(bounds.getWidth() * 0.38);
+    
+    g.setColour(juce::Colours::purple);
+    g.fillRect(outputBounds);
+    
+    auto sliderBounds = bounds.removeFromBottom(bounds.getHeight() / 3);
+    auto releaseBounds = sliderBounds.removeFromRight(sliderBounds.getWidth() / 3);
+    auto ratioBounds = sliderBounds.removeFromRight(sliderBounds.getWidth() / 2);
+    
+    g.setColour(juce::Colours::red);
+    g.fillRect(sliderBounds);
+    g.setColour(juce::Colours::orange);
+    g.fillRect(ratioBounds);
+    g.setColour(juce::Colours::yellowgreen);
+    g.fillRect(releaseBounds);
 
-    g.setColour (juce::Colours::white);
+    g.setColour (juce::Colours::black);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+//    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void PaperCompressorAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    auto bounds = getLocalBounds();
+    bounds = bounds.reduced(bounds.getWidth() / 60);
+    
+    auto outputBounds = bounds.removeFromRight(bounds.getWidth() * 0.38);
+    outputComponent.setBounds(outputBounds);
+    
+    auto sliderBounds = bounds.removeFromBottom(bounds.getHeight() / 3);
+    auto releaseBounds = sliderBounds.removeFromRight(sliderBounds.getWidth() / 3);
+    auto ratioBounds = sliderBounds.removeFromRight(sliderBounds.getWidth() / 2);
+    
+    attackSlider.setBounds(sliderBounds);
+    ratioSlider.setBounds(ratioBounds);
+    releaseSlider.setBounds(releaseBounds);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,6 +10,8 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "Components/OutputComponent.h"
+#include "Components/PaperKnob.h"
 
 //==============================================================================
 /**
@@ -28,6 +30,12 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     PaperCompressorAudioProcessor& audioProcessor;
+    
+    PaperKnob attackSlider, ratioSlider, releaseSlider;
+    
+    juce::AudioProcessorValueTreeState::SliderAttachment attackSliderAttachment, ratioSliderAttachment, releaseSliderAttachment;
 
+    OutputComponent outputComponent;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PaperCompressorAudioProcessorEditor)
 };
