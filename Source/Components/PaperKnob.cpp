47837e7c7b0cf7546ed9c652e99a63a6d0ace89f
FluxMusic <alex.huber743@gmail.com>
2024-06-05 18:28:46 +0200

    fixed paper knob text displayed the parameter only on the ratio, attack and release sliders

--- Source/Components/PaperKnob.cpp
+++ Source/Components/PaperKnob.cpp
@@ -27,43 +27,40 @@ PaperKnob::~PaperKnob()
 
 void PaperKnob::paint(juce::Graphics& g)
 {
-    auto bounds = getLocalBounds();
+    auto fullBounds = getLocalBounds();
+    auto size = juce::jmin(fullBounds.getWidth(), fullBounds.getHeight());
+    auto bounds = juce::Rectangle<int>(size, size);
+    bounds.setCentre(fullBounds.getCentre());
     
     juce::String valueString = "";
     
-    if (param->getParameterID().equalsIgnoreCase("LowCutFreq")
-        || param->getParameterID().equalsIgnoreCase("LowShelfFreq")
-        || param->getParameterID().equalsIgnoreCase("PeakFreq")
-        || param->getParameterID().equalsIgnoreCase("HighShelfFreq")
-        || param->getParameterID().equalsIgnoreCase("HighCutFreq"))
+    if (param->getParameterID().equalsIgnoreCase("InputGain")
+        || param->getParameterID().equalsIgnoreCase("OutputGain"))
     {
-        valueString = static_cast<juce::String>(getValue());
-        valueString.append(" Hz", 3);
-    }
-    else if (param->getParameterID().equalsIgnoreCase("LowShelfGain")
-             || param->getParameterID().equalsIgnoreCase("PeakGain")
-             || param->getParameterID().equalsIgnoreCase("HighShelfGain"))
-    {
-        auto range = getRange();
-        valueString = juce::String::formatted("%.1f", juce::jmap(getValue(), range.getStart(), range.getEnd(), -18.0, 18.0));
+        valueString = juce::String::formatted("%.1f", getValue());
         valueString.append(" dB", 3);
     }
-    else if (param->getParameterID().equalsIgnoreCase("LowShelfQ")
-             || param->getParameterID().equalsIgnoreCase("PeakQ")
-             || param->getParameterID().equalsIgnoreCase("HighShelfQ"))
+    else if (param->getParameterID().equalsIgnoreCase("Ratio"))
     {
-        auto range = getRange();
-        valueString = juce::String::formatted("%.1f", juce::jmap(getValue(), range.getStart(), range.getEnd(), 0.1, 10.0));
+        valueString = juce::String::formatted("%.1f", getValue());
+        valueString.append(":1", 2);
     }
     else
     {
-        if (auto choiceParam = dynamic_cast<juce::AudioParameterChoice*>(param))
-            valueString = choiceParam->getCurrentChoiceName();
+        valueString = static_cast<juce::String>(getValue());
+        valueString.append(" ms", 3);
     }
+    
+    juce::Rectangle<int> labelBounds;
     
     const auto valueBounds = bounds.removeFromBottom(bounds.getHeight() / 5);
     
-//    const auto labelBounds = bounds.removeFromTop(bounds.getHeight() / 4);
+    if (param->getParameterID().equalsIgnoreCase("InputGain")
+        || param->getParameterID().equalsIgnoreCase("OutputGain"))
+    {
+    }
+    else
+        labelBounds = bounds.removeFromTop(bounds.getHeight() / 4);
     
     const auto startAngle = juce::degreesToRadians(180.f + 30.f);
     const auto endAngle = juce::degreesToRadians(180.f - 30.f) + juce::MathConstants<float>::twoPi;
@@ -99,9 +96,13 @@ void PaperKnob::paint(juce::Graphics& g)
                                           endAngle,
                                           *this);
     }
-    g.setColour(juce::Colours::black);
+    if (!param->getParameterID().equalsIgnoreCase("InputGain")
+        || !param->getParameterID().equalsIgnoreCase("OutputGain"))
+    {
+        g.setColour(juce::Colours::black);
+        g.setFont(labelBounds.getHeight() / 1.3);
+        g.drawFittedText(sliderName, labelBounds, juce::Justification::centred, 1);
+    }
     g.setFont(valueBounds.getHeight() / 1.3);
-    g.drawFittedText(sliderName, valueBounds, juce::Justification::centred, 1);
-//    
-//    g.drawFittedText(static_cast<juce::String>(valueString), valueBounds, juce::Justification::centred, 1);
+    g.drawFittedText(static_cast<juce::String>(valueString), valueBounds, juce::Justification::centred, 1);
 }
